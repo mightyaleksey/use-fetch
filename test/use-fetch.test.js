48a8249c1757be7419fecdfd910adbb210c15d4e
Aleksey Litvinov <iamhoneyboy@gmail.com>
2019-06-09 07:19:54 +0300

    update tests

--- test/use-fetch.test.js
+++ test/use-fetch.test.js
@@ -3,9 +3,27 @@
 const test = require('tape')
 const usefetch = require('../src/index')
 
+function to(promise) {
+  return promise
+    .then(d => [null, d])
+    .catch(e => [e])
+}
+
 test('use-fetch -> json -> fetch and parse the response', async (t) => {
   t.plan(1)
 
-  const response = await usefetch('/mock/json', { json: true, retry: 0 })
+  const [, response] = await to(usefetch('/mock/json', { json: true, retry: 0 }))
   t.deepEqual(response.body, { success: 'yes' })
+})
+
+test('use-fetch -> json -> throw ParseError for non-json response', async (t) => {
+  t.plan(4)
+
+  const url = new URL('/mock/ok', window.location.href).toString()
+  const [error] = await to(usefetch('/mock/ok', { json: true, retry: 0 }))
+
+  t.ok(error instanceof usefetch.ParseError)
+  t.equal(error.status, 200)
+  t.equal(error.statusText, 'OK')
+  t.equal(error.url, url)
 })
